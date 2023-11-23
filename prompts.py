7a2dcabe22c73ebf0abd1cae20dc378144f4e931
RichardUG <rsug.rsug@gmail.com>
2023-11-22 20:02:58 -0500

    lab

---
+++ prompts.py
@@ -0,0 +1,21 @@
+from langchain.chains import LLMChain
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+import os
+
+os.environ["OPENAI_API_KEY"] = "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"
+
+
+template = """Question: {question}
+
+Answer: Let's think step by step."""
+
+prompt = PromptTemplate(template=template, input_variables=["question"])
+
+llm = OpenAI()
+llm_chain = LLMChain(prompt=prompt, llm=llm)
+
+question = "What is at the core of Popper's theory of science?"
+
+response = llm_chain.run(question)
+print(response)
